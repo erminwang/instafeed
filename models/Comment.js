2948c32312c4b5d9648e59a832eed5c819b6fe9d
dwa79 <dwa79@sfu.ca>
2017-04-11 22:23:31 -0700

    hi

---
+++ models/Comment.js
@@ -0,0 +1,32 @@
+module.exports = function(sequelize, DataType) {
+    var Comment = sequelize.define('Comment', {
+    	//id is a string to follow sources.json format
+         id: {
+            type: DataType.INTEGER,
+            field: 'id',
+            primaryKey: true,
+            autoIncrement: true
+
+        },
+        description: {
+            type: DataType.STRING,
+            field: 'description'
+    	  },
+        replyid: {
+            type: DataType.INTEGER,
+            field: 'replyid'
+        }
+    },{
+    	  'timestamps':true,
+        classMethods: {
+		    associate: function(models) {
+		    	// each comment belongs to one article
+		        models.Comment.belongsTo(models.Article, {timestamps:false});
+		        // each comment belongs to one user
+		        models.Comment.belongsTo(models.User, {timestamps:false});
+		    }
+		}
+    });
+
+    return Comment;
+};
